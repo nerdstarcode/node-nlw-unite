e85dbb5aa2f3dff27f83ac445d6fca3c1213742f
Sthiven <sthivendev@gmail.com>
2024-04-01 22:25:23 -0300

    :recycle: removes required url on redis

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { z } from 'zod';
 import { RedisClientType, createClient } from 'redis';
 const logger = new Logger('Main');
 export const RedisClientOn: RedisClientType = createClient({
-  url: z.string({ invalid_type_error: 'RedisClient URL is undefined' }).url().parse(process?.env?.REDIS_URL)
+  url: process?.env?.REDIS_URL
 });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  RedisClientOn.on('error', (err) => console.log('Redis client Error', err));
-  await RedisClientOn.connect().then(() => { logger.debug('Connection Started with Redis') });
+  if (process?.env?.REDIS_URL) {
+    RedisClientOn.on('error', (err) => console.log('Redis client Error', err));
+    await RedisClientOn.connect().then(() => { logger.debug('Connection Started with Redis') });
+  }
   await app.listen(3000);
 }
 bootstrap();
